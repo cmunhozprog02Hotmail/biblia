151654bfbc6bf783335a6602d32b63521bba7ccd
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-09-23 14:17:40 -0300

    first commit

---
+++ Biblia/Biblia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Biblia.MvcApplication" Language="C#" %>
---
+++ Biblia/Biblia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Biblia.Startup))]
+namespace Biblia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
